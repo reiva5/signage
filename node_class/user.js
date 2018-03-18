fbafffdb3b1c3011b412be70824d78462848d20e
PratamaAgung <apratamamia@gmail.com>
2018-03-19 01:52:50 +0700

    add function get user

--- node_class/user.js
+++ node_class/user.js
@@ -11,7 +11,20 @@ function User(){
     db = new DB();
 }
 
-
-
+method.getUser = function(id){
+    var conn = db.getConnection();
+    return new Promise( (resolve, reject) => {
+        conn.query("SELECT * FROM user WHERE `user_id`=" + id, function (err, result, field){
+            if (err) 
+                return reject(err);
+            result.forEach(element => {
+                this.id = element.user_id;
+                this.username = element.username;
+                this.password = element.password; 
+            });
+            resolve(this);
+        });
+    });
+}
 
 module.exports = User;

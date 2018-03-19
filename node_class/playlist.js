c5c072e887123b82e37519e0e3989b9800803638
Harum Lokawati <harumlokawati@yahoo.com>
2018-03-19 14:06:38 +0700

    add playlist and playlist_router

---
+++ node_class/playlist.js
@@ -0,0 +1,89 @@
+var DB = require("./db");
+var Promise = require("promise");
+var db;
+var method = Playlist.prototype;
+function Playlist(){
+    this.id = null;
+    this.name = null;
+    db = new DB();
+}
+
+method.getID = function(){
+    return this.id;
+}
+method.getName = function(){
+    return this.name;
+}
+method.setID = function(newID){
+    this.id = newID;
+}
+method.setName = function(newName){
+    this.name = newName;
+}
+
+method.getPlaylist = function(id){
+    var conn = db.getConnection();
+    return new Promise( (resolve, reject) => {
+        conn.query("SELECT * FROM slider_playlist WHERE `id`=" + id, function (err, result, field){
+            if (err) 
+                return reject(err);
+            result.forEach(element => {
+                this.id = element.id;
+                this.name = element.name;
+            });
+            resolve(this);
+        });
+    });
+}
+
+method.addPlaylist = function(name){
+    var conn = db.getConnection();
+    return new Promise( (resolve, reject) => {
+        conn.query("INSERT INTO `slider_playlist`(`name`) VALUES ('" + name
+            + "')", function(err, result, field) {
+            if (err)
+                reject(err);
+            resolve(result);
+        })
+    });
+}
+
+method.editPlaylist = function (id, name) {
+    var conn = db.getConnection();
+    return new Promise( (resolve, reject) => {
+        conn.query("UPDATE `slider_playlist` SET `name` = '" + name + "' WHERE `id` = " + id, 
+            function (err, result, field){
+            if (err)
+                reject(err);
+            resolve(result);
+        });
+    });
+}
+
+method.deletePlaylist= function (id) {
+    var conn = db.getConnection();
+    return new Promise( (resolve, reject) => {
+        conn.query("DELETE FROM `slider_playlist` WHERE `id`=" + id, function(err, result, field){
+            if (err)
+                reject(err);
+            resolve(result);
+        });
+    });
+}
+
+method.getAllPlaylist = function(){
+    var conn = db.getConnection();
+    return new Promise( (resolve, reject) =>{
+        conn.query("SELECT * FROM `slider_playlist`", function(err, result, field){
+            if(err)
+                reject(err);
+            resolve(result);
+        });
+    });
+}
+
+method.destroy = function(){
+    db.disconnect();
+}
+
+module.exports = Playlist;
---
+++ node_class/playlist_router.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var router = express.Router();
+var Playlist = require("./playlist");
+
+router.get("/", function (req, res){
+    res.json({message: "Playlist home"});
+});
+
+router.post("/insert", function(req, res){
+    var playlist = new Playlist();
+    playlist.addPlaylist(req.query.name).then(result =>{
+        if(result.affectedRows > 0){
+            res.json({status: "success"});
+        } else {
+            res.json({status: "error"});
+        }
+        playlist.destroy();
+    });
+});
+
+router.get("/get", function(req, res){
+    var playlist = new Playlist();
+    playlist.getPlaylist(req.query.id).then( result => {
+        console.log(result);
+        if (result) {
+            res.json({
+                name : result.name,
+            });
+        } else {
+            res.json({});
+        }
+        playlist.destroy();
+    });
+});
+router.get("/get_all", function(req, res){
+    var playlist = new Playlist();
+    playlist.getAllPlaylist().then( result => {
+        console.log(result);
+        var all_playlist = [];
+        result.forEach(element => {
+            all_playlist.push({
+                id : element.id,
+                name : element.name
+            });
+        });
+        res.json(all_playlist);
+        playlist.destroy();
+    });
+});
+router.get("/delete", function(req, res){
+    var playlist = new Playlist();
+    playlist.deletePlaylist(req.query.id).then(result =>{
+        if(result.affectedRows > 0){
+            res.json({status: "success"});
+        } else {
+            res.json({status: "failed"});
+        }
+        playlist.destroy();
+    });
+});
+router.get("/update", function(req, res){
+    var playlist = new Playlist();
+    playlist.editPlaylist(req.query.id,req.query.name).then(result =>{
+        if(result.affectedRows > 0){
+            res.json({status: "success"});
+        } else {
+            res.json({status: "failed"});
+        }
+        playlist.destroy();
+    });
+});
+module.exports = router;

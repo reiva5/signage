3bac459345cc25d7762e8a99afb8b638bfc762ac
PratamaAgung <apratamamia@gmail.com>
2018-03-19 16:06:30 +0700

    add routing for admin-dashboard

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(session({secret: 'super secret session'}));
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
-    res.render("index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("\login", function(req, res){
+app.get("/admin_dashboard", function(req, res){
+    if(req.session.time_login){
+        res.sendFile(__dirname + "/public/admin-table.html");
+    } else {
+        res.redirect("/admin");
+    }
+});
+
+app.get("/admin", function(req, res) {
+    if(req.session.time_login){
+        res.redirect("/admin_dashboard");
+    } else {
+        res.sendFile(__dirname + "/public/admin.html");
+    }
+});
+
+app.post("/login", function(req, res){
     var sess = req.session;
-    sess.time_login = new Date.now();
-    res.render("admin-table.html");
-})
+    sess.time_login = Date.now();
+    res.redirect("/admin_dashboard");
+});
+
 app.listen(3000, function(){
     console.log("Apps started on port 3000");
 });

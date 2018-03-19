c6065712cb96531e30d6d9aefa4fceeb46d636d9
PratamaAgung <apratamamia@gmail.com>
2018-03-19 14:52:14 +0700

    add render for index file

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var DB = require("./node_class/db");
+var express = require('express');
+var session = require('express-session');
+var app = express();
 
-var db = new DB();
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(session({secret: 'super secret session'}));
 
-var conn = db.getConnection();
+app.get("/", function(req, res){
+    res.render("index.html");
+});
 
-console.log(conn);
-
-db.disconnect();
+app.post("\login", function(req, res){
+    var sess = req.session;
+    sess.time_login = new Date.now();
+    res.render("admin-table.html");
+})
+app.listen(3000, function(){
+    console.log("Apps started on port 3000");
+});

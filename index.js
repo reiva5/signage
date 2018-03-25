92ada5ae2bc946999ded634b477b5989f941c358
reiva5 <jehiannormansaviero@gmail.com>
2018-03-26 01:45:06 +0700

    Add routes to login

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var session = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(session({secret: 'super secret session'}));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/public/index.html");
@@ -20,17 +23,25 @@ app.get("/admin_dashboard", function(req, res){
 });
 
 app.get("/admin", function(req, res) {
+    console.log("Waktunya adalah: " + req.session.time_login);
     if(req.session.time_login){
         res.redirect("/admin_dashboard");
     } else {
+        console.log("askdjfhsakldfhsakljfhsladkfh");
         res.sendFile(__dirname + "/public/admin.html");
     }
 });
 
 app.post("/login", function(req, res){
-    var sess = req.session;
-    sess.time_login = Date.now();
-    res.redirect("/admin_dashboard");
+    console.log("Bismilllah");
+    if (req.body.username == "jehian" && req.body.password == "12345"){
+        var sess = req.session;
+        console.log("udah lewat sini");
+        sess.time_login = Date.now();
+        res.send('valid');
+    } else {
+        res.send('failed');
+    }
 });
 
 app.listen(3000, function(){

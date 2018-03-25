908e4a8a38ebf1fe29612cd23587f45159edc6dc
PratamaAgung <apratamamia@gmail.com>
2018-03-25 23:33:45 +0700

    Revert "add new index.js"
    This reverts commit d5af0446da2626e6c74900380812ed746ad1ca28.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var session = require('express-session');
 var app = express();
 var Slider = require("./node_class/slider_router");
-var View = require("./node_class/view_router")
 
 app.set('views', __dirname + '/views');
 app.set("view engine", "pug")
+app.engine('html', require('ejs').renderFile);
 app.use(session({
     secret: 'super secret session',
     name : 'signage-cookie',
@@ -14,18 +14,33 @@ app.use(session({
 }));
 app.use(express.static('public'));
 
-app.get("/", function(req, res){
-    res.redirect("/view/index");
+app.get("/render", function(req, res){
+    res.render("index")
+});
+
+app.get("/admin_dashboard", function(req, res){
+    if(req.session.time_login){
+        res.sendFile(__dirname + "/public/admin-table.html");
+    } else {
+        res.redirect("/admin");
+    }
+});
+
+app.get("/admin", function(req, res) {
+    if(req.session.time_login){
+        res.redirect("/admin_dashboard");
+    } else {
+        res.sendFile(__dirname + "/public/admin.html");
+    }
 });
 
 app.post("/login", function(req, res){
     var sess = req.session;
     sess.time_login = Date.now();
-    res.redirect("/view/admin_dashboard");
+    res.redirect("/admin_dashboard");
 });
 
 app.use("/slider", Slider);
-app.use("/view", View);
 
 app.listen(3000, function(){
     console.log("Apps started on port 3000");

3f64141da3db2248cf882232b686f3f9781b1fa2
PratamaAgung <apratamamia@gmail.com>
2018-03-25 20:26:52 +0700

    get all slider to render page

--- node_class/view_router.js
+++ node_class/view_router.js
@@ -7,11 +7,23 @@ router.get("/index", function(req, res){
 });
 
 router.get("/admin_dashboard", function(req, res){
-    if(req.session.time_login){
-        res.render("admin-table");
+    if (req.session.time_login){
+        var slider = new Slider();
+        slider.getAllSlider().then(result => {
+            var array_slider = [];
+            result.forEach(element => {
+                array_slider.push({
+                    slider_id : element.slider_id,
+                    playlist_id : element.playlist_id,
+                    slider_name : element.slider_name,
+                    slider_content : element.slider_content 
+                });
+            });
+            res.render("admin-table", array_slider);
+        });
     } else {
         res.redirect("/view/admin");
-    }
+    } 
 });
 
 router.get("/admin", function(req, res) {

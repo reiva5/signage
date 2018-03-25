c4ae399a5f008e973e12493534df6aeb6b85c6ca
PratamaAgung <apratamamia@gmail.com>
2018-03-25 22:13:35 +0700

    debug get all slider

--- node_class/view_router.js
+++ node_class/view_router.js
@@ -11,28 +11,36 @@ router.get("/admin_dashboard", function(req, res){
         var slider = new Slider();
         slider.getAllSlider().then(result => {
             var array_slider = [];
-            var curr_playlist_id = 1;
+            var curr_playlist_id = 0;
+            var curr_playlist_name = "";
             var playlist_array = []
             result.forEach(element => {
-                if (curr_playlist_id != element.playlist_id) {
-                    array_slider.push({
-                        playlist_id : curr_playlist_id,
-                        content : playlist_array
-                    });
+                if (curr_playlist_id != element.id) {
+                    if (playlist_array.length > 0){
+                        array_slider.push({
+                            playlist_id : curr_playlist_id,
+                            playlist_name : curr_playlist_name,
+                            content : playlist_array
+                        });
+                    }
+                    curr_playlist_id = element.id;
+                    curr_playlist_name = element.name;
                     playlist_array = []
-                    curr_playlist_id += 1;
                 }
                 playlist_array.push({
                     slider_id : element.slider_id,
-                    playlist_id : element.playlist_id,
+                    playlist_id : element.id,
                     slider_name : element.slider_name,
                     slider_content : element.slider_content 
                 });
             });
-            array_slider.push({
-                playlist_id : curr_playlist_id,
-                content : playlist_array
-            });
+            if (playlist_array.length > 0){
+                array_slider.push({
+                    playlist_id : curr_playlist_id,
+                    playlist_name : curr_playlist_name,
+                    content : playlist_array
+                });
+            }
             res.render("admin-table", {playlists: array_slider});
         });
     } else {

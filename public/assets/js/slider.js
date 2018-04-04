621cc146a2ef83fe4b51a3f3193819e3e0d4c413
PratamaAgung <apratamamia@gmail.com>
2018-04-04 23:12:11 +0700

    add entrance word with halo

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     document.getElementById("right-bubble").style.display = "none";
 	document.getElementById("left-bubble").style.display = "none";
 	var final_transcript ="";
-	var bubble_active = false;
+    var bubble_active = false;
+    var active_chat = false;
+    var last_chat = null;
+    var time_now = null;
 
 	if (!('webkitSpeechRecognition' in window)) {
 		alert('Recognition not supported!\n Please use Chrome version 25 or later');
@@ -13,23 +16,24 @@ $(document).ready(function() {
 		recognition.lang = 'id-ID';
 
 		recognition.onstart = function(){
-			console.log("recognizing");
+			// console.log("recognizing");
 		}
 
 		recognition.onerror = function(){
-			console.log("error occured");
+			// console.log("error occured");
 		}
 
 		recognition.onend = function(){
-            console.log("recognition end");
+            // console.log("recognition end");
 			recognition.start();
 		}
 
 		recognition.onspeechstart = function(){
-			console.log("speech start");
-			if (!bubble_active){
+            console.log("speech start");
+            time_now = new Date().getTime();
+			if (!bubble_active && active_chat && time_now - last_chat <= 60*100){
 				document.getElementById("right-bubble").style.display = "block";
-				bubble_active = true;
+                bubble_active = true;
 			}
 		}
 
@@ -39,24 +43,43 @@ $(document).ready(function() {
                 document.getElementById("right-bubble-text").innerHTML = "";
 				document.getElementById("right-bubble").style.display = "none";
                 bubble_active = false;
-                final_transcript = "";
-			    interim_transcript = "";
-			}	
+            }	
+            final_transcript = "";
+			interim_transcript = "";
 		}
 
 		recognition.onresult = function(event){
-			var interim_transcript = "";
-			for (var i = event.resultIndex; i < event.results.length; ++i) {
-		      	if (event.results[i].isFinal) {
-		        	final_transcript += event.results[i][0].transcript;
-                    document.getElementById("right-bubble-text").innerHTML = final_transcript;
-                    console.log(final_transcript);
-		      	} else {
-		        	interim_transcript += event.results[i][0].transcript;
-                    document.getElementById("right-bubble-text").innerHTML = interim_transcript;
-                    console.log(interim_transcript);
-		      	}
-			}
+            interim_transcript = "";
+            if (last_chat == null || time_now - last_chat > 60*100){
+                for (var i = event.resultIndex; i < event.results.length; ++i) {
+                    // console.log("masuk");
+                    if (event.results[i].isFinal) {
+                        final_transcript += event.results[i][0].transcript;
+                        // console.log(final_transcript);
+                        if (final_transcript.toLowerCase() == "halo") {
+                            active_chat = true;
+                            last_chat = time_now;
+                            document.getElementById("left-bubble-text").innerHTML = "Halo, ada yang bisa saya bantu?";
+                            document.getElementById("left-bubble").style.display = "block";
+                        }
+                    }
+                }
+            } else if (active_chat && time_now - last_chat <= 60*100) {
+                for (var i = event.resultIndex; i < event.results.length; ++i) {
+                    // console.log("active");
+                    if (event.results[i].isFinal) {
+                      final_transcript += event.results[i][0].transcript;
+                      document.getElementById("right-bubble-text").innerHTML = final_transcript;
+                    //   console.log(final_transcript);
+                    } else {
+                      interim_transcript += event.results[i][0].transcript;
+                      document.getElementById("right-bubble-text").innerHTML = interim_transcript;
+                    //   console.log(interim_transcript);
+                    }
+                }
+                last_chat = time_now;
+            }
+            console.log(active_chat);
 		}
 		recognition.start();
 		console.log('start');
